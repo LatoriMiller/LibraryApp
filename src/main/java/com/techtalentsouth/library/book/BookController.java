6330c0c924b7c4deab6b642f2503d087ac717a6e
LatoriMiller <latori.miller@gmail.com>
2019-01-30 19:53:05 -0500

    fix controller and refactor html pages

--- src/main/java/com/techtalentsouth/library/book/BookController.java
+++ src/main/java/com/techtalentsouth/library/book/BookController.java
@@ -17,32 +17,28 @@ public class BookController {
 	
 	@GetMapping("/")
 	public ModelAndView index(Book book) {
-		ModelAndView mv = new ModelAndView("index");
+		ModelAndView mv = new ModelAndView("/book/index");
 		mv.addObject("books", bookRepository.findAll());
 		return mv;
 	}
 	
-//	@GetMapping(value= "/")
-//	public String index(Book book) {
-//		return "index";
-//	}
+	@GetMapping("/book/addBook")
+	public ModelAndView showAddBook(Book book) {
+		ModelAndView mv = new ModelAndView("book/addBook");
+		mv.addObject("books", bookRepository.findAll());
+		return mv;
+	}
 	
-//	@PostMapping("/")
-//	public ModelAndView createBook(Book book) {
-//		ModelAndView mv = new ModelAndView("page1");
-//		Book newBook = bookRepository.save(new Book(book.getTitle(), book.getAuthor()));
-//		mv.addObject("newBook", newBook);
-//		return mv;
-//	}
+	@PostMapping("/book/newBook")
+	public ModelAndView createBook(Book book) {
+		ModelAndView mv = new ModelAndView("book/index");
+		mv.addObject("books", bookRepository.findAll());
+		Book newBook = bookRepository.save(new Book(book.getTitle(), book.getAuthor()));
+		mv.addObject("newBook", newBook);
+		return mv;
+	}
 	
-//	@PostMapping(value = "/page1")
-//	public String addNewBook(Book book, Model model) {
-//		bookRepository.save(new Book(book.getTitle(), 
-//									 book.getAuthor()));
-//		model.addAttribute("title", book.getTitle());
-//		model.addAttribute("author", book.getAuthor());
-//		return "results";
-//	}
+
 
 
 
